8f487c3ce9044550c135882429708b693ea55556
Diego Pascual <diegopf@users.noreply.github.com>
2022-04-09 15:03:26 +0200

    chore: configure jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: "ts-jest",
   testMatch: ["<rootDir>/**/*.spec.ts"],
+  testEnvironment: "jsdom",
+  transform: {
+    // process `*.vue` files with `vue-jest`
+    ".*\\.(vue)$": "vue-jest",
+  },
 };
